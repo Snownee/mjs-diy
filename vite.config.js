9d66cfdbaabc582dc48ca32b1e6d6a1aaabbface
Snownee <i@snownee.com>
2026-03-25 19:28:00 +0800

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mjs-diy/',
+  base: process.env.MJS_DIY_BASE_URL || './',
   plugins: [
     vue(),
     vueDevTools(),
